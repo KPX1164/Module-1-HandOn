900ef872e0487229b184c32a32fb56578d536016
Hakushin <kpx1164@gmail.com>
2023-11-27 16:40:50 +0700

    moved resource

--- Module#1/HandOn/main.py
+++ Module#1/HandOn/main.py
@@ -7,7 +7,7 @@ import numpy as np
 # ====================================== Functions ====================================== #
 
 def get_and_clean_data():
-    data = pd.read_csv('resource/software_developer_united_states_1971_20191023_1.csv')
+    data = pd.read_csv('../Resource/software_developer_united_states_1971_20191023_1.csv')
     description = data['job_description']
     cleaned_description = description.apply(lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
     cleaned_description = cleaned_description.apply(lambda s: s.lower())
