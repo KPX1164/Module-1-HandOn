89d8bd99bea98e1a41c5b1834d7bac936a9c47e2
Hakushin <kpx1164@gmail.com>
2023-12-04 20:27:24 +0700

    added all activities

--- Module#2/HandOn2/Limiting1000rows.py
+++ Module#2/HandOn2/Limiting1000rows.py
@@ -7,6 +7,8 @@ import string
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 import timeit
+import matplotlib.pyplot as plt
+
 def get_and_clean_data():
     data = pd.read_csv('../Resource/software_developer_united_states_1971_20191023_1.csv')
     description = data['job_description']
@@ -31,23 +33,23 @@ tokenized_description = cleaned_description.apply(lambda s: word_tokenize(s))
 # remove stop words
 stop_dict = set(stopwords.words())
 sw_removed_description = tokenized_description.apply(lambda s: set(s) - stop_dict)
+# sw_removed_description = tokenized_description.apply(lambda s: list(OrderedSet(s) - stop_dict))
 sw_removed_description = sw_removed_description.apply(lambda s: [word for word in s if len(word) > 2])
 
 
-#create stem caches
+# create stem caches
 concated = np.unique(np.concatenate([s for s in tokenized_description.values]))
 stem_cache = {}
 ps = PorterStemmer()
 for s in concated:
     stem_cache[s] = ps.stem(s)
 
-#stem
+# stem
 stemmed_description = sw_removed_description.apply(lambda s: [stem_cache[w] for w in s])
 
 # print(tokenized_description)
 
 cv = CountVectorizer(analyzer=lambda x: x)
-# vectoriser = cv.fit
 X = cv.fit_transform(stemmed_description)
 print(pd.DataFrame(X.toarray(), columns=cv.get_feature_names_out()))
 
@@ -61,8 +63,9 @@ print(np.shape(X*X.T))
 print(timeit.timeit(lambda: X*X.T, number=1))
 
 
-print(timeit.timeit(lambda: np.matmul(XX, XX.T), number=3)/3)
-print(timeit.timeit(lambda: X.todok()*X.T.todok(), number=3)/3)
-print(timeit.timeit(lambda: X.tolil()*X.T.tolil(), number=3)/3)
-print(timeit.timeit(lambda: X.tocoo()*X.T.tocoo(), number=3)/3)
-print(timeit.timeit(lambda: X.tocsc()*X.T.tocsc(), number=3)/3)
+# print(timeit.timeit(lambda: np.matmul(XX, XX.T), number=3)/3)
+# print(timeit.timeit(lambda: X.todok()*X.T.todok(), number=3)/3)
+# print(timeit.timeit(lambda: X.tolil()*X.T.tolil(), number=3)/3)
+# print(timeit.timeit(lambda: X.tocoo()*X.T.tocoo(), number=3)/3)
+# print(timeit.timeit(lambda: X.tocsc()*X.T.tocsc(), number=3)/3)
+
